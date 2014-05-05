c0ee8e57b7e68d46821cb3e3def25cc65ecd2a36
Kaliyev Zhansar <jansar86@gmail.com>
2014-05-06 01:20:13 +0600

    added new class PriorityType

---
+++ src/kz/amikos/android/shoplist/enums/PriorityType.java
@@ -0,0 +1,18 @@
+package kz.amikos.android.shoplist.enums;
+
+public enum PriorityType {
+	
+	LOW (0),
+	MIDDLE (1),
+	HIGH (2);
+	
+	private int index;
+	
+	PriorityType(int index){
+		this.index = index;
+	}
+	
+	public int getIndex() {
+		return index;
+	}	
+}
